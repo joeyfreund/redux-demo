7632f5721022abdea4d40077acf4bbb7fe88e068
Joey Freund <joeyfreund@gmail.com>
2016-07-13 13:58:52 -0400

    state has two parts, and the reducer only changes one part at a time (different actions = different parts)

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+'use strict';
+
 var Redux = require('redux');
 
 var store = Redux.createStore((state, action) => {
-  // console.log('Reducer is called with ', state, 'and', action);
-  if (state){
-    return state + 1;
-  } else {
-    return 42;
+  if (! state){
+    return { part1: 42, part2: 'a'};
   }
+
+  switch (action.type) {
+    case 'ACTION_1':
+      return {
+        part1 : state.part1 + 1,
+        part2 : state.part2
+      };
+
+    case 'ACTION_2':
+      return {
+        part1 : state.part1,
+        part2 : state.part2 + action.foo
+      };
+    default:
+      return state;
+  }
+
 });
 
 
@@ -15,8 +31,9 @@ console.log('The initial state is', store.getState());
 
 // Dispatch some actions ...
 for (var i = 0; i < 10; i++) {
+  let action_num = (1 + (i % 2));
   setTimeout(() => {
-    store.dispatch({type: 'TEST_ACTION', foo: 'bar'});
+    store.dispatch({type: 'ACTION_' + action_num, foo: 'bar'});
   }, 100 * i);
 }
 
